2be36156337c46b96c80bfc94f12b48be32c86f5
Lucas Clivio <lucas_clivio@hotmail.com>
2024-06-03 22:49:07 -0300

    Agrega archivo de resolucion 1

---
+++ src/main/java/org/grupo05/aliados/logica/Resolucion1.java
@@ -0,0 +1,12 @@
+package org.grupo05.aliados.logica;
+
+public class Resolucion1 {
+
+	public void resolver(String inputfilename) {
+		try {
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
