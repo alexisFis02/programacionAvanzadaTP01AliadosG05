7df12d349e48adf64d071bbb88000221644b1a70
Lucas Clivio <lucas_clivio@hotmail.com>
2024-06-03 22:49:31 -0300

    Inicio implementacion de lectura de archivo

--- src/main/java/org/grupo05/aliados/archivo/Archivo.java
+++ src/main/java/org/grupo05/aliados/archivo/Archivo.java
@@ -1,12 +1,29 @@
 package org.grupo05.aliados.archivo;
 
+import java.io.File;
+import java.util.Scanner;
+
+import org.grupo05.aliados.data.Datos;
+
 public class Archivo {
 
-	public static void abrirArchivo() {
+	public static Datos abrirArchivo(String inputfilename) {
+		File f = new File(inputfilename);
+		try {
+			Archivo.procesarArchivo(f);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.err.println("LOG: error en abrir archivo, " + e.getMessage());
+		}
 		
+		return new Datos();
 	}
 	
-	private static void procesarArchivo() {
+	private static void procesarArchivo(File file) throws Exception{
+		Scanner scan = new Scanner(file);
+		
+		scan.line
 		
 	}
 }
